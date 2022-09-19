d75813402d1490967814d309dd5197de6b12a660
lby132 <quddbscjswow@naver.com>
2022-09-19 19:05:21 +0900

    Eureka client

--- src/main/java/com/example/userservice/UserserviceApplication.java
+++ src/main/java/com/example/userservice/UserserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@EnableEurekaClient
 public class UserserviceApplication {
 
     public static void main(String[] args) {
